d31ae582019e09ea51b2da0d246943f272f21ddb
Huaxuan Zhu <hzhu007@hammer.cs.ucr.edu>
2015-04-16 20:42:22 -0700

    implement single command handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,29 @@
 #include <stdio.h>
 #include <cstring>
 #include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 void display_info();
+char* command_input();
+void execution(char* command);
 
 int main(int argc, char** argv)
 {
-    string input;
     while(1)
     {
         display_info();
-        getline(cin, input);
-        char* input_cstr = new char [input.length()+1];
-        strcpy(input_cstr, input.c_str());
-        char* command = strtok(input_cstr, "#");
-        if(0 == strncmp(command, "exit", sizeof(command)))
+        char* command = command_input();
+        if(0 == strcmp(command, "exit"))
+        //if(0 == strncmp(command, "exit", sizeof(command)))
             break;
+        else
+        {
+            execution(command);
+        }
     }
 
     return 0;
@@ -34,3 +40,42 @@ void display_info()
     printf("[rshell]%s@%s $ ", userName, hostName); //user@host$
 }
 
+char* command_input()
+{
+    string input;
+    getline(cin, input);
+    char* input_cstr = new char [input.length()+1];
+    strcpy(input_cstr, input.c_str());
+    return strtok(input_cstr, "#");
+}
+
+void execution(char* command)
+{
+    char* argv[10000];
+    int i = 0;
+    argv[i] = strtok(command, " ");
+    while(NULL != argv[i])
+    {
+        ++i;
+        argv[i] = strtok(NULL, " ");
+    }
+
+    pid_t pid = fork();
+    if(-1 == pid)
+    {
+        perror("fork() in handle_command()");
+        exit(1);
+    }
+    else if(0 == pid)
+    {
+        if(-1 == execvp(argv[0], argv))
+            perror("execvp() in handle_command()");
+        exit(1);
+    }
+    else
+    {
+        if(-1 == wait(NULL))
+            perror("wait() in handle_command()");
+    }
+    return;
+}
