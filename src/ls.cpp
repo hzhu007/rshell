88ba2c73d212bd25926762cff41af91c5ca2d70d
Huaxuan Zhu <hzhu007@hammer.cs.ucr.edu>
2015-04-27 23:03:25 -0700

    test

--- src/ls.cpp
+++ src/ls.cpp
@@ -25,8 +25,8 @@ static bool addr_input = false;
 
 
 void get_param(int argc, char** argv);
-void long_list_display(char* addr, char* fileName);
-void handle_ls(char* addr);
+void long_list_display(const char* addr, const vector<char*> &files);
+void handle_ls(const char* addr);
 int main(int argc, char** argv)
 {
     get_param(argc, argv);
@@ -34,8 +34,8 @@ int main(int argc, char** argv)
     if(addr_input)
         strcpy(addr, "./");
     else
-        strcpy(addr, "../src");
-    if(addr[strlen(addr-1)] != '/')  //ensure address ends with '/'
+        strcpy(addr, "./");
+    if(addr[strlen(addr)-1] != '/')  //ensure address ends with '/'
         strcat(addr, "/");           //to visit certain file with filename
     cout << addr << endl;
     handle_ls(addr);
@@ -72,7 +72,7 @@ void get_param(int argc, char** argv)
     param_none = !(param_a|param_l|param_R);
 }
 
-void handle_ls(char* addr)
+void handle_ls(const char* addr)
 {
     DIR* dirp;
     vector<char*> files;
@@ -85,7 +85,8 @@ void handle_ls(char* addr)
     int errno = 0;
     while(NULL != (filespecs = readdir(dirp)))
     {
-        files.push_back(filespecs->d_name);
+        if(!(filespecs->d_name[0]=='.' && !param_a))  //if is hidden and not -a, don't print
+            files.push_back(filespecs->d_name);
     }
     if(errno != 0)
     {
@@ -98,32 +99,42 @@ void handle_ls(char* addr)
         exit(1);
     }
     sort(files.begin(), files.end());
-    for(int i = 0; i < files.size(); ++i)
+    if(param_l)
+        long_list_display(addr, files);
+    else
     {
-        if(!(files.at(i)[0]=='.' && !param_a))  //if is hidden and not -a, don't print
-            if(param_l)
-                long_list_display(addr, files.at(i));
-            else
-                cout << files.at(i) << "  ";
+        for(int i = 0; i < files.size(); ++i)
+            cout << files.at(i) << "  ";
+        cout << endl;
     }
 }
 
-void long_list_display(char* addr, char* fileName)
+void long_list_display(const char* addr, const vector<char*> &files)
 {
-    char path[10000];
-    strcpy(path, addr);
-    strcat(path, fileName);
+    vector<char*> path;
+    char pathTemp[1000];
+    for(int i = 0; i < files.size(); ++i)
+    {
+        strcpy(pathTemp, addr);
+        strcat(pathTemp, files.at(i));
+        path.push_back(pathTemp);
+        cout << files.at(i) << " " << path.at(i) << endl;
+    }
     struct stat buf;
-    stat(path, &buf);
-    cout << ((S_ISDIR(buf.st_mode)) ? "d":"-")
-         << ((buf.st_mode & S_IRUSR)? "r":"-")
-         << ((buf.st_mode & S_IWUSR)? "w":"-")
-         << ((buf.st_mode & S_IXUSR)? "x":"-")
-         << ((buf.st_mode & S_IRGRP)? "r":"-")
-         << ((buf.st_mode & S_IWGRP)? "w":"-")
-         << ((buf.st_mode & S_IXGRP)? "x":"-")
-         << ((buf.st_mode & S_IROTH)? "r":"-")
-         << ((buf.st_mode & S_IWOTH)? "w":"-")
-         << ((buf.st_mode & S_IXOTH)? "x":"-");
-    cout << fileName << endl;
+    for(int i = 0; i < files.size(); ++i)
+    {
+        stat(path.at(i), &buf);
+        cout << ((S_ISDIR(buf.st_mode)) ? "d":"-")
+             << ((buf.st_mode & S_IRUSR)? "r":"-")
+             << ((buf.st_mode & S_IWUSR)? "w":"-")
+             << ((buf.st_mode & S_IXUSR)? "x":"-")
+             << ((buf.st_mode & S_IRGRP)? "r":"-")
+             << ((buf.st_mode & S_IWGRP)? "w":"-")
+             << ((buf.st_mode & S_IXGRP)? "x":"-")
+             << ((buf.st_mode & S_IROTH)? "r":"-")
+             << ((buf.st_mode & S_IWOTH)? "w":"-")
+             << ((buf.st_mode & S_IXOTH)? "x":"-") << " ";
+
+        cout << files.at(i) << endl;
+    }
 }
